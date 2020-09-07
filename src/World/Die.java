0e0ca03e0ee873845b1d81469a493a77fe160689
Alialsaeedi1 <alialsaeedi2001@gmail.com>
2020-09-07 14:39:36 +0200

    Die class

--- src/World/Die.java
+++ src/World/Die.java
@@ -1,4 +1,35 @@
 package World;
 
+import java.util.*;
+
 public class Die {
+    private List<int[]> DieList = new ArrayList<int[]>();
+    public Die(){
+
+    }
+    public void generateDie(){
+        int roll1=0;
+        int roll2=0;
+        Random rn = new Random();
+        for(int i=0;i<151;i++){
+        roll1 = rn.nextInt(6) + 1;
+        roll2 = rn.nextInt(6) + 1;
+        int[] list = new int[2];
+        list[0] = roll1;
+        list[1] = roll2;
+        DieList.add(list);  
+        }
+    }
+    public List getDieList(){
+        return DieList;
+    }
+
+    public void printList(){
+        generateDie();
+        System.out.print("{ ");
+        for(int i=0;i<DieList.size();i++){
+            System.out.print(" {" + DieList.get(i)[0]+ "," + DieList.get(i)[1] + "} ");
+        }
+        System.out.print(" }");
+    }
 }
