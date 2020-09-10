e60fcfd881cdb69fc6f7bc7cda07825836d2b666
A-gamil <alaagamil5264@gmail.com>
2020-09-10 17:27:00 +0200

    added corrections to board class

--- src/World/Board.java
+++ src/World/Board.java
@@ -6,8 +6,7 @@ public class Board {
     private Space[] spaces;
     public Board(){
         spaces = new Space[25];//0 is for the "eaten" ,1-12 is the bottom(right-left), 13-24 is top (Left to right)
-        createSide( 1);
-        createSide(2);
+        createSpaces();
         spaces[0]=new Space();
         addPieces(1,2,0);
         addPieces(6,5,1);
@@ -20,16 +19,12 @@ public class Board {
         addPieces(24,2,1);
 
     }
-
-
-
-
+    //methods for board creation
     private void addPieces(int spaceIndex, int num, int colorId) {
         for(int i = 0;i< num;i++)
             spaces[spaceIndex].getPieces().add(new Piece(colorId));
     }
-
-    private void createSide(int placement) {
+    private void createSpaces() {
         for (int i = 0; i < spaces.length; i++) {
             spaces[i] = new Space();
         }
@@ -37,7 +32,8 @@ public class Board {
 
 
     public String toString() {
-//        String[][] resAr= new String [spaces.length][5];
+        //two text representations, one is just strings, the other is a 2d table
+//        String[][] resAr= new String [spaces.length][15];//15 bc thats the maximumnumber of pieces that could exist in a space
 //
 //        for(int i=0;i<spaces.length;i++){
 //            for(int n=0;n<spaces[i].getPieces().size();n++){
@@ -46,7 +42,7 @@ public class Board {
 //        }
 //
 //        return Arrays.deepToString(resAr).replace("], ", "]\n").replace("[[", "[").replace("]]", "]");
-//
+
         String res="";
         for(int i=0;i<spaces.length;i++){
             res+= spaces[i]+ "\n";
