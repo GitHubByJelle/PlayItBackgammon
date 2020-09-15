a19a159d195f3c07848ea3eba9aa5fe08212e7e8
welremco <welremco@gmail.com>
2020-09-15 13:35:02 +0200

    Commented pseudocode for bot measures v1

---
+++ src/AI/Bot.java
@@ -0,0 +1,35 @@
+package AI;
+
+public class Bot {
+//    assume board is structured in list from 1-25
+//    standard start postition
+    public static void main(String[] args) {
+
+    }
+//    public static double OtherPiecesSlain(Board B, Board prevB){
+//        return prevB.getBlackPieces() - B.getBlackPieces();
+//    }
+//    public static double pipScore(Board B){
+//        double pip = 0;
+//        for(int i = 0; i<25; i++){
+//            if(Board.spaces[i].contains("W")){
+//                pip =+ (25-i)*Board.spaces[i].length;
+//            }
+//        }
+//        return -pip;
+//    }
+//    public static double DoneScore(Board B){
+//        return Board.spaces[25].length;
+//    }
+//
+//    public static double piecesAlone(Board B){
+//        double alone = 0;
+//        for(int i = 0; i<25; i++){
+//            if(Board.spaces[i].contains("W")&& Board.spaces[i].length == 1){
+//                alone++;
+//            }
+//        }
+//        return -pip;
+//    }
+
+}
