b8472da8ccb02cf2b1a015677e21c0f7e30eb847
welremco <welremco@gmail.com>
2020-09-15 16:18:02 +0200

    bot measures v0.6 Measures implemented, bot made an object, TODO list added, profile selection added, Evaluation function added and bug fixes.

--- src/AI/Bot.java
+++ src/AI/Bot.java
@@ -3,71 +3,153 @@ import World.*;
 
 public class Bot {
 //    assume board is structured in list from 1-25
-//    standard start postition
+//    standard start position
+    //False is white and True is red
+    //TODO Wait for nextValidMoves to be implemented
+    //TODO Use Evaluation function to select move out of nextValidMoves
+    //TODO Use profiles in bot to make player 1 think 1 move ahead and player 2 2 or 3 moves ahead. Using winning as evolution for bot
+    //TODO Optimize selection out of ValidMoves using GA
+    //
+    public boolean profile;
     public static void main(String[] args) {
         Board b= new Board();
 
         System.out.println(b);
-        System.out.println(pipCount(b));
+        double[] weightsarr = {0.1,2,3,23,2};
+        Bot bot = new Bot(true);
+        System.out.println(bot.pipCount(b));
+    }
+    public Bot(boolean profile){
+        this.profile = profile;
+    }
+    public double EvaluationFunc(Board B, Board prevB, double[] weightsarr){
+        return this.OtherPiecesSlain(B, prevB)*weightsarr[0] + this.pipCount(B)*weightsarr[1] + this.DoneScore(B)*weightsarr[2] + this.DoneBoardScore(B)*weightsarr[3] + this.piecesAlone(B)*weightsarr[4];
     }
     // Number of pieces enemy pieces slain
-    //Positive
-    public static double OtherPiecesSlain(Board B, Board prevB){
+    // Positive
+    public double OtherPiecesSlain(Board B, Board prevB){
         return numberOfEnemyPieces(prevB) - numberOfEnemyPieces(B);
     }
 
     // Count the number of enemy pieces
-    public static double numberOfEnemyPieces(Board B){
-        double numberPieces = 0;
-        for(int i = 0; i<25; i++){
-            for (Piece piece : B.getSpaces()[i].getPieces()){
-                if (piece.getId() == 1){
-                    numberPieces++;
+    public double numberOfEnemyPieces(Board B){
+        if(this.profile) {
+            double numberPieces = 0;
+            for (int i = 0; i < 25; i++) {
+                for (Piece piece : B.getSpaces()[i].getPieces()) {
+                    if (piece.getId() == 0) {
+                        numberPieces++;
+                    }
                 }
             }
+            return numberPieces;
+        }else{
+            double numberPieces = 0;
+            for (int i = 0; i < 25; i++) {
+                for (Piece piece : B.getSpaces()[i].getPieces()) {
+                    if (piece.getId() == 1) {
+                        numberPieces++;
+                    }
+                }
+            }
+            return numberPieces;
         }
-        return numberPieces;
     }
 
     // Calculate the total number of points that the bot must move his pieces to bring them home
     // Negative
-    public static double pipCount(Board B){
-        double pip = 0;
-        for(int i = 0; i<25; i++){
-            for (Piece piece : B.getSpaces()[i].getPieces()){
-                if (piece.getId() == 0){
-                    pip += 25-i;
+    public double pipCount(Board B){
+        if(this.profile) {
+            double pip = 0;
+            for (int i = 1; i < 25; i++) {
+                for (Piece piece : B.getSpaces()[i].getPieces()) {
+                    if (piece.getId() == 1) {
+                        pip += i;
+                    }
                 }
             }
+            if(B.getSpaces()[0].getSize() > 0 && B.getSpaces()[0].getPieces().get(0).getId() == 1){
+                pip += B.getSpaces()[0].getSize()*24;
+            }
+            return -pip;
+        } else {
+            double pip = 0;
+            for (int i = 0; i < 25; i++) {
+                for (Piece piece : B.getSpaces()[i].getPieces()) {
+                    if (piece.getId() == 0) {
+                        pip += 25 - i;
+                    }
+                }
+            }
+            return -pip;
         }
-        return -pip;
     }
     // How many pieces are finished, assuming finished position is at 0.
     // Positive
-    public static double DoneScore(Board B){
-        return B.getSpaces()[0].getSize();
+    public double DoneScore(Board B){
+        if(this.profile) {
+            double numberPieces = 0;
+            for (int i = 0; i < 25; i++) {
+                for (Piece piece : B.getSpaces()[i].getPieces()) {
+                    if (piece.getId() == 1) {
+                        numberPieces++;
+                    }
+                }
+            }
+            return 15 - numberPieces;
+        } else {
+            double numberPieces = 0;
+            for (int i = 0; i < 25; i++) {
+                for (Piece piece : B.getSpaces()[i].getPieces()) {
+                    if (piece.getId() == 0) {
+                        numberPieces++;
+                    }
+                }
+            }
+            return 15 - numberPieces;
+        }
     }
     // How many pieces are in the last board
     // Positive
-    public static double DoneBoardScore(Board B){
-        double score = 0;
-        for(int i = 1; i<7; i++){
-            if(B.getSpaces()[i].getSize() >0 && B.getSpaces()[i].getPieces().get(0).getId() == 1){
-                score =+ B.getSpaces()[i].getSize();
+    public double DoneBoardScore(Board B){
+        if(this.profile) {
+            double score = 0;
+            for (int i = 1; i < 7; i++) {
+                if (B.getSpaces()[i].getSize() > 0 && B.getSpaces()[i].getPieces().get(0).getId() == 1) {
+                    score = +B.getSpaces()[i].getSize();
+                }
             }
+            return score;
+        } else {
+            double score = 0;
+            for (int i = 19; i < 25; i++) {
+                if (B.getSpaces()[i].getSize() > 0 && B.getSpaces()[i].getPieces().get(0).getId() == 0) {
+                    score = +B.getSpaces()[i].getSize();
+                }
+            }
+            return score;
         }
-        return score;
     }
     // How many pieces of your own board are alone
     // Negative
-    public static double piecesAlone(Board B){
-        double alone = 0;
-        for(int i = 0; i<25; i++){
-            if(B.getSpaces()[i].getSize() == 1  && B.getSpaces()[i].getPieces().get(0).getId() == 1){
-                alone++;
+    public double piecesAlone(Board B){
+        if(this.profile) {
+            double alone = 0;
+            for (int i = 0; i < 25; i++) {
+                if (B.getSpaces()[i].getSize() == 1 && B.getSpaces()[i].getPieces().get(0).getId() == 1) {
+                    alone++;
+                }
             }
+            return -alone;
+        } else {
+            double alone = 0;
+            for (int i = 0; i < 25; i++) {
+                if (B.getSpaces()[i].getSize() == 1 && B.getSpaces()[i].getPieces().get(0).getId() == 0) {
+                    alone++;
+                }
+            }
+            return -alone;
         }
-        return -alone;
     }
 
 }
