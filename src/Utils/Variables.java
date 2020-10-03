de609d0671af3377f9f85500ec5f4f7ec92e0408
Alialsaeedi1 <alialsaeedi2001@gmail.com>
2020-10-03 16:55:40 +0200

    color change in Variables.java

--- src/Utils/Variables.java
+++ src/Utils/Variables.java
@@ -10,15 +10,15 @@ public final class Variables {
 
 
     //colors
-    public static Color RED_PIECE_COLOR = Color.RED;//change these later
+    public static Color RED_PIECE_COLOR = Color.BLACK;
     public static Color WHITE_PIECE_COLOR = Color.WHITE;
 
-    public static Color ODD_SPACES_COLOR =new Color(204,153,90);
-    public static Color EVEN_SPACES_COLOR = Color.BLACK;
-    public static Color OUT_OF_PLAY_SPACE_COLOR= Color.DARK_GRAY;
+    public static Color ODD_SPACES_COLOR = new Color(156, 89, 60);
+    public static Color EVEN_SPACES_COLOR = new Color(91, 61, 41);
+    public static Color OUT_OF_PLAY_SPACE_COLOR= new Color(156, 89, 60);
     public static Color RECOLOR_SPACES_COLOR =new Color(250,20,20,50);
 
-    public static Color GAME_BACKGROUND_COLOR = new Color(80,46,41);
+    public static Color GAME_BACKGROUND_COLOR = new Color(213, 165, 117);
 
     //fornow its gonna be like this, later we can change it to store AI objects @ali
     public static String[] PLAYERS= {"Human", "BotA", "BotB"};
