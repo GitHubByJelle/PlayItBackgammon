8632088dc5be75093d4768c6750f928cc1de6a95
markymarkwu <56248896+markymarkwu@users.noreply.github.com>
2020-10-07 12:56:21 +0800

    Update GameOverPanel.java

--- src/GUI/GameOverPanel.java
+++ src/GUI/GameOverPanel.java
@@ -1,6 +1,41 @@
 package GUI;
 
-import javax.swing.*;
+import java.awt.GridLayout;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
-public class GameOverPanel extends JPanel {
+public class GameOverPanel extends JPanel{
+
+	JTextField tf;
+	JButton restart;
+	JButton exit;
+
+	GameOverPanel(){
+		this.setLayout(new GridLayout(3,1));
+		tf = new JTextField();
+		tf.setEditable(false);
+		restart = new JButton("Play again");
+		exit = new JButton("Exit");
+		exit.addMouseListener(new MouseAdapter() {
+			public void mouseClicked(MouseEvent e) {
+				System.exit(0);
+			}
+		});
+		restart.addMouseListener(new MouseAdapter() {
+			public void mouseClicked(MouseEvent e) {
+				//
+			}
+		});
+		this.add(tf);
+		this.add(restart);
+		this.add(exit);
+	}
+
+	public void updateResult(String player) {
+		tf.setText("player " + player + "won");
+	}
 }
