bf50be2c27f6267d6e52088d2ddad9d7c576f3fb
Alialsaeedi1 <alialsaeedi2001@gmail.com>
2020-10-15 13:54:45 +0200

    Change in GameOver Panel design

--- src/GUI/GameOverPanel.java
+++ src/GUI/GameOverPanel.java
@@ -8,6 +8,9 @@ import java.awt.GridLayout;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import javax.swing.*;
+import javax.swing.border.LineBorder;
+
+import java.awt.*;
 
 public class GameOverPanel extends JPanel{
 
@@ -17,10 +20,27 @@ public class GameOverPanel extends JPanel{
 
 	public GameOverPanel(JFrame frame){
 		this.setLayout(new GridLayout(3,1));
+		Color buttonColor = new Color(138, 69, 0);
 		tf = new JTextField();
 		tf.setEditable(false);
+		tf.setBackground(buttonColor);
+		tf.setBorder(new LineBorder(Color.BLACK));
+		tf.setFont(new Font("Segoe UI", Font.PLAIN, 50));
+		tf.setForeground(Color.BLACK);
+		
+
 		restart = new JButton("Play again");
 		exit = new JButton("Exit");
+        restart.setBackground(buttonColor);
+        restart.setBorder(new LineBorder(Color.BLACK));
+		restart.setFont(new Font("Segoe UI", Font.PLAIN, 50));
+		restart.setForeground(Color.BLACK);
+		
+		exit.setBackground(buttonColor);
+        exit.setBorder(new LineBorder(Color.BLACK));
+        exit.setFont(new Font("Segoe UI", Font.PLAIN, 50));
+		exit.setForeground(Color.BLACK);
+
 		exit.addMouseListener(new MouseAdapter() {
 			public void mouseClicked(MouseEvent e) {
 				System.exit(0);
@@ -49,5 +69,6 @@ public class GameOverPanel extends JPanel{
 
 	public void updateResult(String player) {
 		tf.setText(player + " won");
+		tf.setHorizontalAlignment(JLabel.CENTER);
 	}
 }
