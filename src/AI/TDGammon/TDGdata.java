c2e23d34ffb732beff05776b5cd5ef81934b77d9
A-gamil <alaagamil5264@gmail.com>
2021-01-05 15:37:47 +0100

    tddata

--- src/AI/TDGammon/TDGdata.java
+++ src/AI/TDGammon/TDGdata.java
@@ -11,18 +11,20 @@ public class TDGdata {
     public static void main(String[] args){
         Board b=new Board();
         TDGdata d= new TDGdata(b);
-        //d.print();
+        d.print();
     }
 
 
     double [] data= new double[4*27*2];//features
     public TDGdata(Board b){
-        Space[]space = new Space[27];
-        for(int i=0; i<space.length-1;i++){
+        Space[]space = new Space[26];
+        for(int i=0; i<space.length;i++){
             space[i]=b.getSpaces()[i];
         }
-        space[space.length-1]=b.getOutOfPlay();
+        Space out=b.getOutOfPlay();
         int s=0;
+        int pieceCount=0;
+
 
         System.out.println("Space Length: " + space.length);
 
@@ -30,14 +32,15 @@ public class TDGdata {
         //26 is out space
         //1-24 are play spaces
         for(int id=0;id<2;id++) {
-            for (int i = 0; i < space.length; i++) {
-                s = 24 * 4 * id + 4 * i;
+            int i=0;
+            for ( i = 0; i < space.length; i++) {
+                s = 27 * 4 * id + 4 * i;
                 //if w/r pieces
                 if (!space[i].isEmpty()) {
                     //the space is not empty
                     //0 id is for white
                     if (space[i].getPieces().get(0).getId() == id) {
-                        s = 24 * 4 * id + 4 * i;
+                        s = 27 * 4 * id + 4 * i;
                         System.out.println(space[i].getPieces().size());
                         if (space[i].getPieces().size() >= 1)
                             data[s] = 1;
@@ -51,15 +54,46 @@ public class TDGdata {
                         }
                     }
                 }
-                System.out.println(data[s] + " " + data[s + 1] + " " + data[s + 2] + " " + data[s + 3]);
+                System.out.println(space[i].getId()+" "+ s+" "+ data[s] + " " + data[s + 1] + " " + data[s + 2] + " " + data[s + 3]);
                 System.out.println("____________________");
             }
+            System.out.println(i);
+            s = 27 * 4 * id + 4 * (i);
+            pieceCount = countID(out, id);
+            if (pieceCount >= 1)
+                data[s] = 1;
+            if (pieceCount >= 2)
+                data[s+1] = 1;
+            if (pieceCount >= 3)
+                data[s+2] = 1;
+            if (pieceCount >3)
+                data[s+3] = (pieceCount - 3) / 2.0;
+
+            System.out.println(i+" "+ s+" "+ data[s] + " " + data[s + 1] + " " + data[s + 2] + " " + data[s + 3]);
             System.out.println("Last number of id " + id + " is " + (s+3) + "\n");
             System.out.println("Should be " + (4*27+id*4*27 - 1) + "\n");
         }
     }
 
+
+    private int countID(Space s, int id){
+        int res =0;
+        if(s.isEmpty()) return 0;
+        else{
+            for(int i=0;i<s.getPieces().size();i++){
+                if(s.getPieces().get(i).getId()==id) ++res;
+            }
+        }
+        return res;
+    }
     public void print(){
-        stream(data).forEach(System.out::println);
+        //stream(data).forEach(System.out::println);
+
+        for(int i=0;i< data.length;i++){
+            System.out.println(i+" " +data[i]);
+            if(i==108){
+                System.out.println("________________");
+            }
+        }
     }
 }
