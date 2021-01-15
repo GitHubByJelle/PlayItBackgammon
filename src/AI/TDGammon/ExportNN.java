676ae7caece8e3594c285e730aedf2b59065301b
A-gamil <alaagamil5264@gmail.com>
2021-01-15 15:08:03 +0100

    added default file path

--- src/AI/TDGammon/ExportNN.java
+++ src/AI/TDGammon/ExportNN.java
@@ -6,8 +6,9 @@ public class ExportNN {
          * Saves the network to a local file
          * @param target_network NeuralNet target_network object
          */
+        public static String default_NN_path = "src//AI//TDGammon//SavedNN//";
         public static void exportNetwork(NeuralNet target_network,String fileName) throws IOException {
-            ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(fileName));
+            ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(default_NN_path+fileName));
             oos.writeObject(target_network);
             oos.flush();
             oos.close();
--- src/AI/TDGammon/ImportNN.java
+++ src/AI/TDGammon/ImportNN.java
@@ -3,8 +3,9 @@ import java.io.*;
 
 
 public class ImportNN {
+    public static String default_NN_path = "src//AI//TDGammon//SavedNN//";
     public static NeuralNet importNet(String network) throws IOException, ClassNotFoundException {
-        ObjectInputStream ois = new ObjectInputStream(new FileInputStream(network));
+        ObjectInputStream ois = new ObjectInputStream(new FileInputStream(default_NN_path+network));
         NeuralNet net = (NeuralNet) ois.readObject();
         ois.close();
 
